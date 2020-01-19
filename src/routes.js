03e38932a1324a6fa8871662d37432eb903c9531
Gabriel Rodrigues <gabriel.rodrigues@mutantbr.com>
2020-01-19 17:30:22 -0300

    Feature to search dev

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const { Router } = require('express');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController')
 
 const routes = Router();
 
-routes.post('/devs', (req, res) => {
-  const { github_username } =  req.body;
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index)
 
 module.exports = routes;
